bd668d8538d30c9774c4c505e22ddc7503e38a85
LS BoTz <91888887+Adithyanadii@users.noreply.github.com>
2022-01-24 19:22:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,7 +96,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('search here movie', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('updates', url='https://t.me/josprojects'),
+            InlineKeyboardButton('MYdev👨‍💻', url='https://t.me/Monuten007'),
             InlineKeyboardButton('movie club', url='https://t.me/+y53tWFUw6Q43NzE9')
             ],[
             InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
